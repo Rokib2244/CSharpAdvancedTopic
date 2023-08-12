c0d17c7e2f64ea41ac05c4df1b5f1198c3f741df
Rokibul <rokib2244@gmail.com>
2023-08-12 12:13:31 +0600

    Delegate Topic Project Added

---
+++ Delegates/Delegates/PhotoProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+        public void Process(string path, PhotoFilterHandler photoFilterHandler)
+        {
+            var photo = Photo.Load(path); 
+            photoFilterHandler(photo);
+            photo.Save();
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Delegates;
+
+//Console.WriteLine("Hello, World!");
+var processor = new PhotoProcessor();
+var philters = new PhotoFilters();
+PhotoProcessor.PhotoFilterHandler photoFilterHandler = philters.ApplyBrightness;
+photoFilterHandler += RemoveRedEyeFilter;
+//photoFilterHandler += philters.ApplyContrast;
+processor.Process("Image",photoFilterHandler);
+
+static void RemoveRedEyeFilter(Photo photo){
+    Console.WriteLine("Apply RemoveRedEye");
+}
